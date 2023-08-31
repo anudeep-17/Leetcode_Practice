cce6f320187b73a5829386e1d7cead72fe9698eb
Satwik Bhasin <93511515+satwikbhasin@users.noreply.github.com>
2023-08-30 23:14:20 -0400

    Optimized Marked

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,24 @@
+// Optimized
+class Solution {
+    public boolean isPalindrome(int x) {
+        if( x < 0){
+            return false;
+        }
+
+        int reverse = 0;
+        int temp = x;
+
+        while (temp != 0){
+            int digit = temp % 10;
+            reverse = reverse * 10 + digit;
+            temp /= 10;
+        }
+
+        return x == reverse;
+    }
+}
+
+
 // Brute Force
 class Solution {
     public boolean isPalindrome(int x) {
