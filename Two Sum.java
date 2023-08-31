79f2e5a892e1b3995f1a9ce75a927f73ef0e994c
Satwik Bhasin <93511515+satwikbhasin@users.noreply.github.com>
2023-08-30 22:52:08 -0400

    Brute Force & Optimized  Marked

--- Two Sum.java
+++ Two Sum.java
@@ -1,3 +1,4 @@
+// Optimized
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] sol = new int[2];
@@ -15,5 +16,24 @@ class Solution {
         }
         return sol;
 
+    }
+}
+
+// Brute Force
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] sol = new int[2];
+         for(int i = 0; i <nums.length; i++){
+             for(int j = i; j<nums.length; j++){
+                 if(i != j){
+                     if(nums[i] + nums[j] == target){
+                         sol[0] = i;
+                         sol[1] = j;
+                         return sol;
+                     }
+                 }
+             }
+         }
+         return sol;
     }
 }
