3858ab2610823121af0222568ae05a11636b5f9b
Satwik Bhasin <93511515+satwikbhasin@users.noreply.github.com>
2023-09-01 15:55:15 -0400

    Create Remove Duplicates from Sorted Array.java

---
+++ Remove Duplicates from Sorted Array.java
@@ -0,0 +1,14 @@
+# Optimized
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int pointer = 0;
+        for (int i=1; i<nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                pointer++;
+                nums[pointer] = nums[i];
+            }
+            }
+
+        return pointer+1;
+        }
+    }
