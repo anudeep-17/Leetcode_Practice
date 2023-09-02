033cadecb7da923bfdb8e8a0a6882d0c71f678d8
Satwik Bhasin <93511515+satwikbhasin@users.noreply.github.com>
2023-09-01 20:06:35 -0400

    Optimized Marked

--- Search Insert Position.java
+++ Search Insert Position.java
@@ -1,4 +1,26 @@
-# Bruteforce
+# Optimized - O(log n)
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       int start = 0;
+       int end = nums.length - 1;
+
+       while( start <= end){
+            int mid = (start + end)/2;
+
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                end = mid - 1;
+            else if(nums[mid] < target)
+                start = mid + 1;
+       }
+
+       return start;
+    }
+}
+
+
+# Bruteforce - O(n)
 class Solution {
     public int searchInsert(int[] nums, int target) {
         for(int i = 0; i<nums.length; i++){
