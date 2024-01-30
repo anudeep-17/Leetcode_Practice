33c7b36ec1f4bedca36bd4185a35c6921dd4c812
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-01-30 00:33:35 -0500

    Time: 17 ms (6.64%) | Memory: 46 MB (5.17%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -30,6 +30,7 @@ class Solution {
             }
         
             scores[i] = (lastoccurence+1) * mat.length + i;
+            System.out.println(scores[i] + " " + lastoccurence + " " + mat.length + " " + i);
         }
         
         Arrays.sort(scores);
