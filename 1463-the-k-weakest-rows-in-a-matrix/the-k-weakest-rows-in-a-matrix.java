d44cb5d40b635201a764528d98e8ec7871a2d582
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-01-30 00:29:16 -0500

    Time: 1 ms (99.23%) | Memory: 44.9 MB (24.96%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -4,8 +4,6 @@ class Solution {
     public int[] kWeakestRows(int[][] mat, int k) 
     {
         int[] returner = new int[k];
-        Map<Integer, Integer> rowtosolider = new HashMap<>();
-        PriorityQueue<Integer> numberofsoliders = new PriorityQueue<>();
         int[] scores = new int[mat.length];
 
         for(int i = 0; i<mat.length; i++)
@@ -33,25 +31,7 @@ class Solution {
         
             scores[i] = (lastoccurence+1) * mat.length + i;
         }
-
-
-        // for(int i = 0; i< k ; i++)
-        // {
-        //     if(!numberofsoliders.isEmpty())
-        //     {
-        //         int value  = numberofsoliders.poll();
-        //         for (Map.Entry<Integer, Integer> entry : rowtosolider.entrySet()) 
-        //         {
-        //             if (entry.getValue() == value) 
-        //             {
-        //                 returner[i] = entry.getKey();
-        //                 rowtosolider.remove(entry.getKey());
-        //                 break; // Break out of the loop after removing the first occurrence
-        //             }
-        //         }
-        //     }
-        // }
-
+        
         Arrays.sort(scores);
         for( int i = 0; i<k; i++)
         {
