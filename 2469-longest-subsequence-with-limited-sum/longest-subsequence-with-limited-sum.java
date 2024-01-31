557522b30d74bcef41ada79fe6f2abaa190f576d
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-01-30 23:19:42 -0500

    Time: 5 ms (99.06%) | Memory: 44.8 MB (39.79%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
@@ -1,14 +1,4 @@
 class Solution {
-    public int[] prefixsum(int[] n)
-    {
-        int len = n.length;
-        for(int i = 1; i<len; i++)
-        {
-            n[i] = n[i-1] + n[i];
-        }
-        return n;
-    }
-
     public int binarysearch(int n[], int target)
     {
         int left= 0; 
@@ -32,12 +22,15 @@ class Solution {
     public int[] answerQueries(int[] n, int[] queries) 
     {
         Arrays.sort(n);
-        int[] summed = prefixsum(n);
+        for(int i = 1; i<n.length; i++)
+        {
+            n[i] = n[i-1] + n[i];
+        }
         int[] returner = new int[queries.length];
 
         for(int i = 0; i<queries.length; i++)
         {
-            returner[i] = binarysearch(summed, queries[i]);
+            returner[i] = binarysearch(n, queries[i]);
         }
         return returner;
 
