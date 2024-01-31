79d8406693e15ea6d3f10eb29cfc664353e6a76e
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-01-31 15:56:42 -0500

    Time: 9 ms (13.36%) | Memory: 41.7 MB (6.19%) - LeetSync

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int specialArray(int[] nums) 
+    {
+        Arrays.sort(nums);
+        int special = -1;
+
+        for(int i = 1; i <= nums.length; i++)
+        {
+            int left = 0; 
+            int right = nums.length -1;
+            int firstoccurence = -1;
+
+            while(left<=right)
+            {
+                int mid = left + (right - left)/2;
+                if(nums[mid] >= i)
+                {
+                    firstoccurence = mid;
+                    right = mid - 1;
+                }
+                else 
+                {
+                    left = mid + 1;
+                }
+            }
+
+            if(nums.length - firstoccurence == i)
+            {
+               System.out.println(i + " " + firstoccurence);
+               special = i;
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        return special;
+        
+    }
+}
