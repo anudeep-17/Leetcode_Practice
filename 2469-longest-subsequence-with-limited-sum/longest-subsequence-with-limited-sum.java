cd33032e40261cf85357fe6bedc62c2a52424c7a
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-01-30 23:18:44 -0500

    Time: 5 ms (99.06%) | Memory: 44.6 MB (61.85%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
@@ -1,19 +1,45 @@
 class Solution {
-    public int[] answerQueries(int[] array, int[] queries) {
-        Arrays.sort(array);
-        int[] result = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            int sum = 0;
-            int j = 0;
-            for (j = 0; j < array.length; j++) {
-                if (sum + array[j] > queries[i]) {
-                    result[i] = j;
-                    break;
-                }
-                sum += array[j];
-            }
-            if (j == array.length) result[i] = array.length;
+    public int[] prefixsum(int[] n)
+    {
+        int len = n.length;
+        for(int i = 1; i<len; i++)
+        {
+            n[i] = n[i-1] + n[i];
         }
-        return result;
+        return n;
+    }
+
+    public int binarysearch(int n[], int target)
+    {
+        int left= 0; 
+        int right = n.length - 1;
+        int result = 0;
+        while(left<=right)
+        {
+            int mid = left + (right - left)/2;
+            if (n[mid] <= target) 
+            {
+                left = mid + 1;
+            }
+            else 
+            {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public int[] answerQueries(int[] n, int[] queries) 
+    {
+        Arrays.sort(n);
+        int[] summed = prefixsum(n);
+        int[] returner = new int[queries.length];
+
+        for(int i = 0; i<queries.length; i++)
+        {
+            returner[i] = binarysearch(summed, queries[i]);
+        }
+        return returner;
+
     }
 }
