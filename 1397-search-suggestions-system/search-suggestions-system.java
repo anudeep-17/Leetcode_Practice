c1bf39e484ff14b94b340795317157a36de079d0
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-02-04 01:36:59 -0500

    Time: 90 ms (23.24%) | Memory: 47.7 MB (52.82%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.java
+++ 1397-search-suggestions-system/search-suggestions-system.java
@@ -25,44 +25,16 @@ class Solution {
         return firstoccurence;
     }
 
-    public int LastOccurenceIndex(String[] products, String letter)
-    {   
-        int left = 0;
-        int right = products.length -1;
-        int lastoccurence = 0;
-
-        while(left<=right)
-        {
-            int mid = left + (right - left)/2;
-            if(products[mid].substring(0,0).compareTo(letter) == 0)
-            {
-                lastoccurence = mid;
-                left = mid+1;
-            }
-            else if(products[mid].substring(0,0).compareTo(letter) < 0)
-            {
-                left = mid + 1;
-            }
-            else
-            {
-                right = mid - 1;
-            }
-        }
-        return lastoccurence;
-    }
-
-
     public List<List<String>> suggestedProducts(String[] products, String searchWord) 
     {
         List<List<String>> searchhistory = new ArrayList<List<String>>();
         Arrays.sort(products);
         int first =  FirstOccurenceIndex(products, searchWord.substring(0,0));
-        int last = LastOccurenceIndex(products, searchWord.substring(0,0));
-        
+
         for(int j = 0; j< searchWord.length(); j++ )
         {
             List<String>temp = new ArrayList<String>();
-            for(int i = first ; i<= last; i++)
+            for(int i = first ; i<= products.length -1; i++)
             {
                 if(products[i].startsWith(searchWord.substring(0,j+1)))
                 {
