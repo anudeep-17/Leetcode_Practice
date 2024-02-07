9c6bace50704910f89462b40379dece78cd00b68
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-02-07 14:15:03 -0500

    Time: 0 ms (100.00%) | Memory: 44.6 MB (67.35%) - LeetSync

---
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.java
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    List<Integer> treeorder = new ArrayList<>();   
+    public List<Integer> postorder(Node root) 
+    {
+        if(root == null)
+        {
+            return treeorder;
+        }
+
+        if (root.children != null) {
+            for (Node child : root.children) {
+                postorder(child);
+            }
+            treeorder.add(root.val);
+        }
+   
+        return treeorder;
+        
+    }
+}
